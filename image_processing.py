60907f7697f7fc92968d8c02d37063c9f1348566
Guilherme Felipetto <guilherme.g.felipetto@gmail.com>
2024-07-08 18:31:52 -0300

    Adicionando scripts para treinamento, carregamento e uso do modelo treinado. Adicionado nova classe em image_processing.py para logica de carregamento de imagens para uso do modelo treinado

--- image_processing.py
+++ image_processing.py
@@ -1,8 +1,9 @@
-import cv2
-import mediapipe as mp
-import numpy as np
-import json
 import os
+import cv2
+import json
+import numpy as np
+import mediapipe as mp
+from joblib import load
 
 class FaceMeshModel:
     def __init__(self, config_path):
@@ -38,7 +39,7 @@ class FaceMeshModel:
                 dist = self.calculate_distance(i, i + 1) / self.reference_distance
                 file.write(f'{i} {i+1} {dist}\n')
 
-    def calculate_and_draw_distance(self, idx1, idx2, save=False, measure_type='None'):
+    def calculate_and_draw_distance(self, idx1, idx2, save=False, measure_type='None', output_filename=None):
         if not self.points:
             raise ValueError("No landmarks available. Process an image first.")
         
@@ -52,8 +53,9 @@ class FaceMeshModel:
         if save:
             directory = "specific_distances"
             os.makedirs(directory, exist_ok=True)
-            image_basename = os.path.splitext(os.path.basename(self.image_path))[0]
-            output_filename = f"{directory}/{image_basename}_distances.txt"
+            if output_filename is None:
+                image_basename = os.path.splitext(os.path.basename(self.image_path))[0]
+                output_filename = f"{directory}/{image_basename}_distances.txt"
             measure_type = measure_type.replace(' ', '_').strip()
             with open(output_filename, 'a') as file:
                 file.write(f'{idx1} {idx2} {dist} {measure_type}\n')
@@ -97,3 +99,74 @@ class FaceMeshModel:
             cv2.imshow('Facial Landmarks', self.image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+
+
+class Image:
+    def __init__(self, path, facemesh_model, model_path):
+        self.path = path
+        self.modelo_face_mesh = facemesh_model
+        self.modelo_predicao = load(model_path)
+        self.imagem = None
+        self.features = None
+
+    def carregar_imagem(self):
+        self.imagem = cv2.imread(self.path)
+        if self.imagem is not None:
+            print("Imagem carregada com sucesso!")
+            return True
+        else:
+            print("Erro ao carregar imagem. Verifique o caminho.")
+            return False
+
+    def predict(self):
+        if self.carregar_imagem():
+            self.modelo_face_mesh.process_image(self.path)
+
+            self.features = [
+                self.modelo_face_mesh.calculate_and_draw_distance(10, 152),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 152),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 10),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 361),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 132),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 365),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 150),
+                self.modelo_face_mesh.calculate_and_draw_distance(334, 443),
+                self.modelo_face_mesh.calculate_and_draw_distance(282, 334),
+                self.modelo_face_mesh.calculate_and_draw_distance(105, 223),
+                self.modelo_face_mesh.calculate_and_draw_distance(52, 105),
+                self.modelo_face_mesh.calculate_and_draw_distance(54, 284),
+                self.modelo_face_mesh.calculate_and_draw_distance(9, 10),
+                self.modelo_face_mesh.calculate_and_draw_distance(133, 463),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 159),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 386),
+                self.modelo_face_mesh.calculate_and_draw_distance(159, 386),
+                self.modelo_face_mesh.calculate_and_draw_distance(133, 33),
+                self.modelo_face_mesh.calculate_and_draw_distance(263, 362),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 278),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 48),
+                self.modelo_face_mesh.calculate_and_draw_distance(0, 1),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 16),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 61),
+                self.modelo_face_mesh.calculate_and_draw_distance(1, 291),
+                self.modelo_face_mesh.calculate_and_draw_distance(0, 16),
+                self.modelo_face_mesh.calculate_and_draw_distance(10, 33),
+                self.modelo_face_mesh.calculate_and_draw_distance(10, 133),
+                self.modelo_face_mesh.calculate_and_draw_distance(10, 263),
+                self.modelo_face_mesh.calculate_and_draw_distance(10, 362),
+                self.modelo_face_mesh.calculate_and_draw_distance(33, 152),
+                self.modelo_face_mesh.calculate_and_draw_distance(133, 152),
+                self.modelo_face_mesh.calculate_and_draw_distance(152, 263),
+                self.modelo_face_mesh.calculate_and_draw_distance(152, 362)
+            ]
+
+            # Obtenha as probabilidades das previsões
+            probabilidades = self.modelo_predicao.predict_proba([self.features])[0]
+            classe_predita = self.modelo_predicao.predict([self.features])[0]
+            
+            # Encontre o índice da classe com a maior probabilidade
+            max_index = np.argmax(probabilidades)
+            max_probability = probabilidades[max_index] * 100
+
+            print(f"Predição: Elemento: {classe_predita}, {max_probability:.2f}%")
+        else:
+            print("Imagem não está carregada ou erro na carga.")
